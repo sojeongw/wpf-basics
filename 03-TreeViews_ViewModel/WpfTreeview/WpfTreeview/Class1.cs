bdaa7ffe83b20b0741ed37db56360c8547af5e6c
sojeongw <redthink90@gmail.com>
2019-02-01 17:40:28 +0900

    Install Fody

--- 03-TreeViews_ViewModel/WpfTreeview/WpfTreeview/Class1.cs
+++ 03-TreeViews_ViewModel/WpfTreeview/WpfTreeview/Class1.cs
@@ -1,18 +1,44 @@
 ﻿// view model을 위한 class
 
+using PropertyChanged;
 using System.ComponentModel;
 using System.Threading.Tasks;
 
 namespace WpfTreeview
 {
+    //[ImplementPropertyChanged]
     public class Class1 : INotifyPropertyChanged
     {
+
+        private string mTest;
+
         // 이 코드만 있으면 버튼엔 WpfTreeview.Class1 이라고 출력된다.
         public event PropertyChangedEventHandler PropertyChanged = (sender, e) => { };
 
         // xaml에서 binding 요소 지정을 위한 코드
         // Test에 담긴 데이터로 지정했기 때문에 아래에 override한 데이터는 나오지 않음.
-        public string Test { get; set; } = "My Property";
+        // getter setter를 이용해 바로 위에서 만든 event를 가지고 Test안에 있는 값을 반영하게 된다.
+        /*public string Test {
+            get
+            {
+                return mTest;
+            }
+
+            set
+            {
+                if(mTest == value)
+                {
+                    return;
+                }
+
+                mTest = value;
+                PropertyChanged(this, new PropertyChangedEventArgs(nameof(Test)));
+            }
+
+        }*/
+
+        // fody를 쓰면서 자동으로 데이터를 get/set 시켜줌
+        public string Test { get; set; }
 
         public Class1()
         {
@@ -24,7 +50,12 @@ namespace WpfTreeview
             {
                 // 1초에 5번씩 value를 가져온다.
                 await Task.Delay(200);
+
+                // 여기만 바꿔주면 UI에 바로 반영된다.
                 Test = (i++).ToString();
+
+                // Test 에 담긴 값이 실시간으로 표시되는지 확인하는 테스트 코드
+                // PropertyChanged(this, new PropertyChangedEventArgs("Test"));
             }
             });
         }
